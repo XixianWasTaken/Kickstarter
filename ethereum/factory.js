05149f869a6fa267754fd312b9d5bd576eb75410
Xixian Yang <xixianyang@outlook.com>
2022-03-28 17:23:39 -0700

    contract update

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json'
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xdC78724E8Cd1dC47ed194C7A8682f8c54d3d02a1'
+    '0x79700496DaE2364b1179C4F52aBb86c3FAA4C3E2'
 )
 
 export default instance
