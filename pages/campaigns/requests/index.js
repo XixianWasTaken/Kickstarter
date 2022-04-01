a0375b7869708d0aac2453f3e255c35945a4ac93
Xixian Yang <xixianyang@outlook.com>
2022-04-01 04:23:09 -0700

    request form build

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RequestsIndex extends Component {
+    render() {
+        return (
+            <h3>Requests Index</h3>
+        )
+    }
+}
+
+export default RequestsIndex
